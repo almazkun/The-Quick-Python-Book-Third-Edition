40dd3bf13205d6153034048dfb271da43d7ee12b
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-04-10 16:05:14 +0600

    chapte 6

--- Practice and exercises/Ch06/Ch06_lab_strings.py
+++ Practice and exercises/Ch06/Ch06_lab_strings.py
@@ -1,6 +1,9 @@
-text = []
 with open('moby_01.txt') as infile, open('moby_01_clean', 'w') as outfile:
-    for line in infile:
+    for word in infile:
+        word.lower()
+        text = word.translate('-')
+
+text
         # text lowercased
         # delete poctuations
         # split
