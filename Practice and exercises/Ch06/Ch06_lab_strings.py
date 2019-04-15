8d9d811b604d86b6ba15ca1ca7c72a6f814a7f04
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-04-15 16:55:18 +0600

    still chapter 6

--- Practice and exercises/Ch06/Ch06_lab_strings.py
+++ Practice and exercises/Ch06/Ch06_lab_strings.py
@@ -1,10 +1,12 @@
+text = []
 with open('moby_01.txt') as infile, open('moby_01_clean.txt', 'w') as outfile:
     for word in infile:
         word.lower()
-        text = word.translate('-')
+
+        text = text.append(word.lower().rstrip())
 
 text
-        # text lowercased
+
         # delete poctuations
         # split
         # make rows
