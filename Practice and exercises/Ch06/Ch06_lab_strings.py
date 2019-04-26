ff7c0393e90d62b0b646540c71ff924584da8d27
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-04-26 11:13:00 +0600

    Chapter 6 Lab work

--- Practice and exercises/Ch06/Ch06_lab_strings.py
+++ Practice and exercises/Ch06/Ch06_lab_strings.py
@@ -1,13 +1,10 @@
-text = []
+punct = str.maketrans("", "", "!.,:;-?")
+
+
 with open('moby_01.txt') as infile, open('moby_01_clean.txt', 'w') as outfile:
     for word in infile:
-        word.lower()
-
-        text = text.append(word.lower().rstrip())
-
-text
-
-        # delete poctuations
-        # split
-        # make rows
+        cleaned_line = word.lower()
+        cleaned_line = cleaned_line.translate(punct)
+        words = cleaned_line.split()
+        cleaned_words = '\n'.join(words) + '\n'
         outfile.write(cleaned_words)
