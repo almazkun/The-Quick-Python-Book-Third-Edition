baf4db3492db57d6682957a7a37437f08a95aec7
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-05-03 11:27:46 +0600

    Chapter 8 lab work refactored

--- Practice and exercises/Ch08/Word_count.py
+++ Practice and exercises/Ch08/Word_count.py
@@ -1,14 +1,14 @@
 """Reads a file and returns count of lines, words and characters"""
-
-infile = open('word_count.tst')
-lines = infile.read().split('\n')
-line_count = len(lines)
+line_count = 0
 word_count = 0
 char_count = 0
 
-for line in lines:
-    words = line.split()
-    word_count += len(words)
-    char_count += len(line)
+with open('word_count.tst') as infile:
+    for line in infile:
+        line_count += 1
+        char_count += len(line)
+        words = line.split()
+        word_count = len(words)
+
 
 print('File has {0} lines, {1} words, {2} characters'.format(line_count, word_count, char_count))
